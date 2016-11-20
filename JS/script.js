d86848fc2a98060b584d5e7b23e82787a645d069
BayodeOlorundare <bayodeolorundare@gmail.com>
2016-11-20 00:29:42 -0500

    Update script.js
    Updates to JS file to include:
    -previous and next video button
    -show/hide description
    -related video link from BC cloud

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,21 @@
 var myPlayer,
-	playlistIds = ["4868197176001","4868197203001","4868197200001","4890340678001","4868197201001"],
+	playlistIds = ["Enter playlist ID","Enter playlist ID","Enter playlist ID","Enter playlist ID","Enter playlist ID"],
 	playlistIdsLength = playlistIds.length,
-	tabs = document.getElementsByClassName("brightcove-vidhub-button"),
+	tabs = document.getElementsByClassName("BCP-vidhub-button"),
 	currentTab,
 	currentTabName,
 	currentvideo,
 	playlistNames = [];
 
 //Once the player is ready, it will load the first (latest) playlist	
-videojs("brightcove-videohub-player").ready(function(){
+videojs("BCP-videohub-player").ready(function(){
 	myPlayer = this;
 	processTab(0);
 	loadplaylistentries();
 	//View the current video object
 	myPlayer.on('loadstart',function(){				
-		//console.log('mediainfo', myPlayer.mediainfo);
+		//console.log('current video info', myPlayer.mediainfo);
+		//console.log('playlist info', myPlayer.playlist());
 	});		
 });
 //This loads the selected playlist on the page
@@ -47,6 +48,20 @@ function loadPlaylistply (currentId) {
 		myPlayer.catalog.load(playlist);
 	});
 };
+//Loads next video in playlist
+function nextVideo() {
+	var nextVidObject = myPlayer.playlist.next();
+	$('#BCP-next-title').html(myPlayer.playlist()[myPlayer.playlist.currentItem()+1].name);
+	$('#BCP-previous-title').html(myPlayer.playlist()[myPlayer.playlist.currentItem()-1].name);
+	console.log("next video loading");	
+};
+//Loads previous video in playlist
+function previousVideo() {
+	var previousVidObject = myPlayer.playlist.previous();
+	console.log("previous video loading");
+	$('#BCP-previous-title').html(myPlayer.playlist()[myPlayer.playlist.currentItem()-1].name);
+	$('#BCP-next-title').html(myPlayer.playlist()[myPlayer.playlist.currentItem()+1].name);
+};
 //This reloads the tabs
 function resetTabs () {
 	var i,
@@ -58,17 +73,35 @@ function resetTabs () {
 };
 //This loads all metadata
 function loadmetadata() {
-	var playerdata = videojs('brightcove-videohub-player').ready(function(){
+	var playerdata = videojs('BCP-videohub-player').ready(function(){
 		myPlayer = this;
 		myPlayer.on('loadstart',function(){
 			//video category populates
-			$('#brightcove-vidhub-vidcat').html(myPlayer.mediainfo.custom_fields.category);
+			$('#BCP-vidhub-vidcat').html(myPlayer.mediainfo.custom_fields.category);
+			//next video title in playlist
+			if(myPlayer.playlist.currentItem() != myPlayer.playlist().length-1){
+				$('#BCP-next-span').show();
+				$('#BCP-next-title').html(myPlayer.playlist()[myPlayer.playlist.currentItem()+1].name);
+				$('#BCP-next-category').html(myPlayer.mediainfo.custom_fields.category);
+			}else{
+				$('#BCP-next-span').hide();
+				//$('#BCP-next-title,#BCP-next-category').html(" ");
+			};
+			//previous video title in playlist
+			if(myPlayer.playlist.currentItem() > 0){
+				$('#BCP-prev-span').show();
+				$('#BCP-previous-title').html(myPlayer.playlist()[myPlayer.playlist.currentItem()-1].name);
+				$('#BCP-previous-category').html(myPlayer.mediainfo.custom_fields.category);
+			}else{
+				$('#BCP-prev-span').hide();
+				//$('#BCP-previous-title,#BCP-previous-category').html(" ");				
+			};
 			//video title populates
-			$('#brightcove-vidhub-meta-title').html(myPlayer.mediainfo.name);
+			$('#BCP-vidhub-meta-title').html(myPlayer.mediainfo.name);
 			//video duration populates
-			$('#brightcove-vidhub-meta-time').html(fmtMSS(Math.round(myPlayer.mediainfo.duration)));
+			$('#BCP-vidhub-meta-time').html(fmtMSS(Math.round(myPlayer.mediainfo.duration)));
 			//video description populates
-			$('#brightcove-vidhub-meta-desc').html(myPlayer.mediainfo.long_description);
+			$('#BCP-vidhub-meta-desc').html(myPlayer.mediainfo.long_description);
 			//converting date to MMM DD, YYYY
 			var month = new Array();
 			month[0] = "January";
@@ -83,16 +116,17 @@ function loadmetadata() {
 			month[09] = "October";
 			month[10] = "November";
 			month[11] = "December";
-			var d = myPlayer.mediainfo.created_at.slice(6,7),
-				m = month[d-1],
+			var d1 = myPlayer.mediainfo.created_at.slice(6,7),
+			    d2 = myPlayer.mediainfo.created_at.slice(5,6),
+				m = month[d2+d1-1],
 				d = myPlayer.mediainfo.created_at.slice(8,10),
 				y = myPlayer.mediainfo.created_at.slice(0,4);
 			//video date populates
-			$('#brightcove-vidhub-meta-date').html('&nbsp;&nbsp;|' + '&nbsp;&nbsp;'+m +' '+ d+', ' + y);
-			//video sharing link; checks if value is null and will place blank value or value from catalogue
+			$('#BCP-vidhub-meta-date').html('Published on '+m +' '+ d+', ' + y);
+			//video sharing link
 			if(myPlayer.mediainfo.link == null){
-				$('#brightcove-vidhub-sharing').html("");
-			}else{$('#brightcove-vidhub-sharing').html("<br><a target='_blank' //href="+myPlayer.mediainfo.link.url+">"+myPlayer.mediainfo.link.text+"</a>");};			
+				$('#BCP-vidhub-sharing').html("");
+			}else{$('#BCP-vidhub-sharing').html("<br><a target='_blank' //href="+myPlayer.mediainfo.link.url+">"+myPlayer.mediainfo.link.text+"</a>");};
 		});
 		//convert the duration seconds into mins/secs
 		function fmtMSS(s){return(s-(s%=60))/60+(9<s?':':':0')+s}
@@ -101,19 +135,18 @@ function loadmetadata() {
 	//scroll to top after thumbnail is clicked
 	$('#vjs-playlist').click(function() {
 		$('html, body').animate({
-			scrollTop: $('#brightcove-vidhub-top').offset().top
+			scrollTop: $('#BCP-vidhub-top').offset().top
 		 }, 600);
 	});
 };
-//Loads more playlist entries onto screen; default 3
+//Loads more playlist entries onto screen
 function loadplaylistentries() {
 	var limit_vids = 6,
 	load_more_vids = 3;
 	//reset the limit_vids
-	$('.brightcove-vidhub-button').click(function() {
+	$('.BCP-vidhub-button').click(function() {
 		limit_vids = 6;	
-	});
-	//controls the speed of how new entries load and hides load button when no more entries available 
+	});	
 	$('#loadMore').bind('click', function(){
 		limit_vids += load_more_vids;
 		$('#vjs-playlist li:lt('+(limit_vids)+')').show(600);
@@ -123,8 +156,22 @@ function loadplaylistentries() {
 	});
 };
 //Video description show/hide
-$(".brightcove-vidhub-arrow-down").hide();
-$(".brightcove-vidhub-opthead").click(function(){
-	$(".brightcove-vidhub-meta-desc").slideToggle(500);
-	$(this).find(".brightcove-vidhub-arrow-down, .brightcove-vidhub-arrow-up").toggle();
+//$(".BCP-vidhub-arrow-down").hide();
+//$(".BCP-vidhub-opthead").click(function(){
+	//$(".BCP-vidhub-meta-desc").slideToggle(500);
+	//$(this).find(".BCP-vidhub-arrow-down, .BCP-vidhub-arrow-up").toggle();
+//});
+$(".BCP-vidhub-opthead").click(function() {
+	var $A1 = $(".BCP-vidhub-show");
+    $(".BCP-vidhub-meta-desc").slideToggle(500);
+	$('.BCP-vidhub-show').text(function(_, text) {
+        return text === 'SHOW LESS' ? 'SHOW MORE' : 'SHOW LESS';
+    })
+});
+//Styling
+$(".BCP-next-video").hover(function(){
+	$(".BCP-next-video, button#BCP-next-button, .BCP-next-title, span#BCP-next-title").toggleClass("BCP-blue-hoverstate");
+});
+$(".BCP-previous-video").hover(function(){
+	$(".BCP-previous-video, #BCP-previous-button, span#BCP-previous-title, .BCP-previous-title").toggleClass("BCP-blue-hoverstate");
 });
